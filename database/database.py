63fe255e786d3e182b2d3c3eca6e0b75cb2c2e34
a.pushkin <a.v.pushkin@gmail.com>
2022-11-23 12:59:42 +0300

    update

--- database/database.py
+++ database/database.py
@@ -3,25 +3,38 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-# class Gender(Base):
-#     __tablename__ = 'gender'
-#     id = sq.Column(sq.Integer, primary_key=True)
-#     name = sq.Column(sq.String(length=20), unique=True)
+
+def drop_tables(engine) -> None:
+    Base.metadata.drop_all(engine)
+
+
+def create_tables(engine) -> None:
+    Base.metadata.create_all(engine)
+
+def status_filler(session) -> None:
+    session.add(Status(name='favorite'))
+    session.add(Status(name='black list'))
+    session.commit()
+
+class Status(Base):
+    __tablename__ = 'status'
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
 
 class User(Base):
     __tablename__ = 'user'
     id = sq.Column(sq.Integer, primary_key=True)
     vk_id = sq.Column(sq.Integer, unique=True)
-    stage = sq.Column(sq.Integer)
 
 
-# class User(Base):
-#     __tablename__ = 'user'
-#     id = sq.Column(sq.Integer, primary_key=True)
-#     name = sq.Column(sq.String(length=20), unique=False)
-#     surname = sq.Column(sq.String(length=20), unique=False)
-#     vk_id = sq.Column(sq.Integer, unique=True)
-#     gender = sq.Column(sq.String(length=20), sq.ForeignKey('gender.id'))
+class Preferences(Base):
+    __tablename__ = 'preferences'
+    id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.Integer, sq.ForeignKey('user.vk_id'))
+    watched_vk_id = sq.Column(sq.Integer)
+    status_id = sq.Column(sq.Integer, sq.ForeignKey('status.id'))
+
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,109 +2,169 @@ from dotenv import load_dotenv
 import os
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from vk.vk import send_message
+from threading import Thread
 from pprint import pprint
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import datetime
+from database.database import Status, User, Preferences, drop_tables, create_tables, status_filler
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+user_dict = {}
+
 load_dotenv()
-engine = sq.create_engine(os.getenv("DSN"))
-Base.metadata.drop_all(engine) # дропаем
-Base.metadata.create_all(engine) # создаём
-Session = sessionmaker(bind=engine)
-session = Session()
+
+vk_group_session = vk_api.VkApi(token=os.getenv('GROUP_TOKEN'))
+vk_user_session = vk_api.VkApi(token=os.getenv('USER_TOKEN'))
+vk_group = vk_group_session.get_api()
+vk_user = vk_user_session.get_api()
+longpoll = VkBotLongPoll(vk_group_session, group_id=os.getenv('GROUP_ID'))
+
+welcome_keyboard = VkKeyboard(one_time=True)
+regular_keyboard = VkKeyboard()
+welcome_keyboard.add_button(label="Начать", color=VkKeyboardColor.POSITIVE)
+welcome_keyboard.add_button(label="Мои данные", color=VkKeyboardColor.POSITIVE)
+regular_keyboard.add_button(label="Дальше", color=VkKeyboardColor.POSITIVE)
+regular_keyboard.add_button(label="В избранное", color=VkKeyboardColor.POSITIVE)
+
+
+def preview_photos(user_photo_list: list) -> list:
+    preview_photo_list = [
+        {'photo_id': photo['id'], 'likes': photo['likes']['count'],
+         'photo_link': [sizes['url'] for sizes in photo['sizes']][-1]}
+        for photo in user_photo_list["items"]]
+    preview_photo_list.sort(key=lambda dictionary: dictionary['likes'])
+    link_list = [[link['photo_id'], link['likes'], link['photo_link']] for link in preview_photo_list[-3:]]
+    return link_list
+
+
+def get_photo(found_user_id: int) -> list:
+    photo_list = vk_user.photos.get(owner_id=found_user_id, album_id="profile", extended=1)
+    return photo_list
+
+
+def send_message(session, vk_id: int, text: str, attachment=None, keyboard=None) -> None:
+    session.method('messages.send',
+                   {'user_id': vk_id, 'message': text, 'random_id': 0, 'keyboard': keyboard, 'attachment': attachment})
+
+
+def profile_info(vkinder_user_id: int) -> dict:
+    user = vk_group_session.method('users.get', {'user_ids': vkinder_user_id,
+                                                 'fields': 'sex, bdate, city, relation'})
+    name = f'{user[0]["first_name"]} {user[0]["last_name"]}'
+    sex = user[0]['sex']
+    if 'bdate' in user[0]:
+        if len(user[0]['bdate'].split('.')) == 3:
+            age = datetime.date.today().year - int(user[0]['bdate'].split('.')[-1])
+        else:
+            age = ''
+    else:
+        age = ''
+    relation = user[0]['relation']
+    city = user[0]['city']['id'] if 'city' in user[0] else ''
+    city_title = user[0]['city']['title'] if 'city' in user[0] else ''
+    return {'name': name, 'sex': str(sex), 'age': str(age), 'relation': str(relation), 'city': str(city),
+            'city_title': city_title}
+
+
+def listener(self_id, session):
+    session.add(User(vk_id=self_id))
+    session.commit()
+
+    vkinder_user_info = profile_info(self_id)
+    vkinder_user_name = vkinder_user_info['name']
+    vkinder_user_city = vkinder_user_info['city']
+    vkinder_user_city_title = vkinder_user_info['city_title']
+    vkinder_user_sex = vkinder_user_info['sex']
+    vkinder_user_age = vkinder_user_info['age']
+
+    count = 100
+    age_step = 3
+    found_user = vk_user.users.search(count=count, sex='1' if vkinder_user_sex == '2' else '2',
+                                      city=vkinder_user_city, age_from=str(int(vkinder_user_age) + age_step),
+                                      age_to=str(int(vkinder_user_age) + age_step), has_photo='1',
+                                      status='6', fields="city, bdate, sex")
+
+
+
+
+    for user in found_user['items']:
+        # еще недоделанная проверка
+        # request_blacklist = session.query(Preferences).filter_by(vk_id=self_id, status_id=2).all()
+        # black_list = [x for x in request_blacklist]
+        # if user["id"] in black_list:
+        #     send_message(vk_group_session, self_id, "попался аккаунт из черного списка, ищем дальше...")
+
+        if user['is_closed']:
+            send_message(vk_group_session, self_id, "попался закрытый аккаунт, ищем дальше...")
+            continue
+
+        user_photo_list = vk_user.photos.get(owner_id=user["id"], album_id="profile", extended=1)
+        if user_photo_list['count'] > 2:
+            user_photos = preview_photos(user_photo_list)
+            send_message(vk_group_session, self_id, f'{user["first_name"]} {user["last_name"]}\n'
+                                                    f'https://vk.com/id{user["id"]}\n',
+                         f'photo{user["id"]}_{user_photos[0][0]},'
+                         f'photo{user["id"]}_{user_photos[1][0]},'
+                         f'photo{user["id"]}_{user_photos[2][0]}', keyboard=regular_keyboard.get_keyboard())
+        else:
+            send_message(vk_group_session, self_id, "у пользователя недостаточно фото, ищем...")
+            continue
+
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                if self_id == event.obj.message["from_id"]:
+                    if event.obj.message["text"].lower() == "дальше":
+                        send_message(vk_group_session, event.obj.message["from_id"], text="ожидай...",
+                                     keyboard=regular_keyboard.get_keyboard())
+                        break
+
+                    if event.obj.message["text"].lower() == "в избранное":
+                        session.add(Preferences(vk_id=self_id, watched_vk_id=user["id"], status_id=1))
+                        session.commit()
+                        send_message(vk_group_session, event.obj.message["from_id"], text="ожидай...",
+                                     keyboard=regular_keyboard.get_keyboard())
+                        break
+
+                    elif event.obj.message["text"] == "выход":
+                        send_message(vk_group_session, event.obj.message["from_id"], text="выходим")
+                        user_dict[event.obj.message["from_id"]] = 1
+                        break
+
 
 def main():
-    vk_group_session = vk_api.VkApi(token=os.getenv('GROUP_TOKEN'))
-    vk_user_session = vk_api.VkApi(token=os.getenv('USER_TOKEN'))
-    vk_group = vk_group_session.get_api()
-    vk_user = vk_user_session.get_api()
-    longpoll = VkBotLongPoll(vk_group_session, group_id=os.getenv('GROUP_ID'))
+
+    engine = sq.create_engine(os.getenv('DSN'))
+    drop_tables(engine)
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    status_filler(session)
 
     for event in longpoll.listen():
-
         if event.type == VkBotEventType.MESSAGE_NEW:
-            self_id = event.obj.message["from_id"]
-            print(f'Для меня от: {event.obj.message["from_id"]}')
-            print('Текст:', event.obj.message["text"])
-            if event.obj.message["text"] == "привет":
-                vk_group_session.method('messages.send',
-                                  {'user_id': event.obj.message["from_id"], 'message': "привет", 'random_id': 0,
-                                   'keyboard': None, 'attachment': None})
-            if event.obj.message["text"] == "пока":
-                vk_group_session.method('messages.send',
-                                  {'user_id': event.obj.message["from_id"], 'message': "пока", 'random_id': 0,
-                                   'keyboard': None, 'attachment': None})
-            if event.obj.message["text"] == "я":
-                vk_group_session.method('messages.send',
-                                  {'user_id': event.obj.message["from_id"], 'message': f'твой ID: {self_id}', 'random_id': 0,
-                                   'keyboard': None, 'attachment': None})
 
-            if event.obj.message["text"] == "найти кандидата":
+            if event.obj.message["from_id"] not in user_dict:
+                send_message(vk_group_session, event.obj.message["from_id"], text="Для начала работы нажмитне начать.",
+                             keyboard=welcome_keyboard.get_keyboard())
+                user_dict[event.obj.message["from_id"]] = 1
 
-                request = vk_user.users.search(count=1, sex='1', has_photo='1', status='6', fields="city, bdate, sex")
-                vk_group_session.method('messages.send',
-                                        {'user_id': event.obj.message["from_id"], 'message': "нашли, но не покажем", 'random_id': 0,
-                                         'keyboard': None, 'attachment': None})
+            if event.obj.message["text"].lower() == "мои данные" and user_dict[event.obj.message["from_id"]] == 1:
+                self_id = event.obj.message["from_id"]
+                send_message(vk_group_session, event.obj.message["from_id"], text=self_id,
+                             keyboard=welcome_keyboard.get_keyboard())
 
-            if event.obj.message['text'] == "логика":
-                vk_group_session.method('messages.send',
-                                        {'user_id': event.obj.message["from_id"], 'message': "вошли в логику",
-                                         'random_id': 0,
-                                         'keyboard': None, 'attachment': None})
-                for event in longpoll.listen():
-                    if event.type == VkBotEventType.MESSAGE_NEW:
-                        if self_id == event.obj.message["from_id"]:
-                            if event.obj.message["text"] == "да":
-                                send_message(vk_group_session, event.obj.message["from_id"], text="да")
+            elif event.obj.message["text"].lower() == "начать" and user_dict[event.obj.message["from_id"]] == 1:
+                self_id = event.obj.message["from_id"]
+                send_message(vk_group_session, event.obj.message["from_id"], text="вошли в поиск")
+                user_dict[event.obj.message["from_id"]] = 2
+                inner_listen = Thread(target=listener, args=(self_id, session))
+                inner_listen.start()
 
-                            else:
-                                send_message(vk_group_session, event.obj.message["from_id"], text="нет")
-                                break
-
-            else:
-                send_message(vk_group_session, event.obj.message["from_id"], text="неизвестно")
+            # else:
+            #     if user_dict[event.obj.message["from_id"]] == 1:
+            #         send_message(vk_group_session, event.obj.message["from_id"], text="неизвестно")
 
 
-
-        elif event.type == VkBotEventType.MESSAGE_REPLY:
-            print('Новое сообщение:')
-
-            print('От меня для: ', end='')
-
-            print(event.obj.peer_id)
-
-            print('Текст:', event.obj.text)
-            print()
-
-        elif event.type == VkBotEventType.MESSAGE_TYPING_STATE:
-            print('Печатает ', end='')
-
-            print(event.obj.from_id, end=' ')
-
-            print('для ', end='')
-
-            print(event.obj.to_id)
-            print()
-
-        elif event.type == VkBotEventType.GROUP_JOIN:
-            print(event.obj.user_id, end=' ')
-
-            print('Вступил в группу!')
-            print()
-
-        elif event.type == VkBotEventType.GROUP_LEAVE:
-            print(event.obj.user_id, end=' ')
-
-            print('Покинул группу!')
-            print()
-
-        else:
-            print(event.type)
-            print()
-
-session.close()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
